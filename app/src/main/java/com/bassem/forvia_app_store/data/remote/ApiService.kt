3f652ca9ca8a563c7db1b5e4d82373cc14983c45
Bassem <BasemOmar2012@gmail.com>
2024-12-18 22:41:16 +0200

    create NetWorkModule.kt and api service

--- app/src/main/java/com/bassem/forvia_app_store/data/remote/ApiService.kt
+++ app/src/main/java/com/bassem/forvia_app_store/data/remote/ApiService.kt
@@ -1,9 +1,10 @@
 package com.bassem.forvia_app_store.data.remote
 
+import com.bassem.forvia_app_store.data.models.AppsResult
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET("listApps/")
-   suspend fun getAppsList(): ApiResult
+   suspend fun getAppsList(): AppsResult
 }
