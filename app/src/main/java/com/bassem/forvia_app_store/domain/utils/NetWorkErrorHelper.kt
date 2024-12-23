32aed2064f7910f6ad40d147276f6ff9bf0de734
Bassem <BasemOmar2012@gmail.com>
2024-12-23 23:37:36 +0200

    add kdoc for getExceptionMessage

--- app/src/main/java/com/bassem/forvia_app_store/domain/utils/NetWorkErrorHelper.kt
+++ app/src/main/java/com/bassem/forvia_app_store/domain/utils/NetWorkErrorHelper.kt
@@ -5,6 +5,20 @@ import com.google.gson.JsonParseException
 import java.io.IOException
 import java.sql.SQLException
 
+/**
+ * Returns a corresponding error type based on the provided exception.
+ *
+ * This function maps specific exceptions to predefined error types for better error handling.
+ * If the exception is not explicitly handled, it defaults to a generic error type containing the exception message.
+ *
+ * @param e The exception to be processed.
+ * @return An [ErrorTypes] instance representing the mapped error type.
+ *
+ * @see IOException
+ * @see SQLException
+ * @see JsonParseException
+ * @see ErrorTypes
+ */
 fun getExceptionMessage(e: Throwable) = when (e) {
     is IOException -> ErrorTypes.IoException
     is SQLException -> ErrorTypes.SqlException
