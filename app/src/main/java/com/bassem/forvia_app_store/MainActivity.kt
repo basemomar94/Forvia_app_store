cd89a3586afafc0768d3fdac8e70f1e461d33f7a
basemomar94 <basemomar2012@gmail.com>
2024-12-24 16:02:47 +0200

    add top app bar with back button functionality

--- app/src/main/java/com/bassem/forvia_app_store/MainActivity.kt
+++ app/src/main/java/com/bassem/forvia_app_store/MainActivity.kt
@@ -18,8 +18,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
         if (hasNotificationPermission()) {
             logger.i("App has notification permission")
             scheduleDailyWork()
